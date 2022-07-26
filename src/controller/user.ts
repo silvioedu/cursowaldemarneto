f828963c17bccc1a4ba4d4685ef440daa7cddf8e
Silvio Silva <silvioedu@gmail.com>
2022-07-26 07:49:13 -0300

    adicionado users/me

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,6 +1,7 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { BaseController } from '@src/controller/base';
 import logger from '@src/logger';
+import { authMiddleware } from '@src/middleware/auth';
 import { User } from '@src/model/user';
 import AuthService from '@src/services/auth';
 import { Request, Response } from 'express';
@@ -37,5 +38,19 @@ export class UserController extends BaseController {
     }
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if(!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!'
+      });
+    }
+    return res.send({ user });
   }
 }
