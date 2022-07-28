aa099d56e343d5709e4999851fd7940f3b9c309c
Silvio Silva <silvioedu@gmail.com>
2022-07-28 09:07:02 -0300

    ordenando por rating

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -5,6 +5,7 @@ import { Beach } from '@src/model/beach';
 import { ForecastProcessingInternalError } from '@src/services/forecast-error';
 import { BeachForecast, TimeForecast } from '@src/services/forecast-interface';
 import { Rating } from '@src/services/rating';
+import _ from 'lodash';
 
 export class Forecast {
   constructor(
@@ -15,23 +16,30 @@ export class Forecast {
   public async processForecastForBeaches(
     beaches: Beach[]
   ): Promise<TimeForecast[]> {
-    const pointsWithCorrectSources: BeachForecast[] = [];
     logger.info(`Preparing the forecast for ${beaches.length} beaches`);
     try {
-      for (const beach of beaches) {
-        const rating = new this.RatingService(beach);
-        const points: ForecastPoint[] = await this.stormGlass.fetchPoints(
-          beach.lat,
-          beach.lng
-        );
-        const enrichedBeachData = this.enrichBeachData(points, beach, rating);
-        pointsWithCorrectSources.push(...enrichedBeachData);
-      }
-      return this.mapForecastByTime(pointsWithCorrectSources);
+      const beachForecast = await this.calculateRating(beaches);
+      const timeForecast = this.mapForecastByTime(beachForecast);
+      return timeForecast.map(t => ({
+        time: t.time,
+        forecast: _.orderBy(t.forecast, ['rating'], ['desc'])
+      }));
     } catch (error) {
       logger.error(error);
       throw new ForecastProcessingInternalError((error as Error).message);
     }
+  }
+
+  private async calculateRating(beaches: Beach[]): Promise<BeachForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+    logger.info(`Preparing the forecast for ${beaches.length} beaches`);
+    for (const beach of beaches) {
+      const rating = new this.RatingService(beach);
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = this.enrichBeachData(points, beach, rating);
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+    return pointsWithCorrectSources;
   }
 
   private enrichBeachData(
